9f4290ef01e2663a834adf1a44163485a7ac8ea6
Brian Gonzalez <me@briangonzalez.org>
2017-03-22 17:48:06 -0700

    author support

--- api/content.js
+++ api/content.js
@@ -33,5 +33,23 @@ if (!gistId) {
   throw new Error(`No Gist ID found in config or via ENV variable.`)
 }
 
-router.get('/content', requestProxy(opts))
+// router.get('/content', requestProxy(opts))
+router.get('/content', (req, res) => {
+  res.send({
+    files: [
+      {
+        content: `
+Lorem ipsum dolor sit amet, minim molestie argumentum est at, pri legere torquatos instructior ex. Vis id odio atomorum oportere, quem modo fabellas sit at, dicat semper est ne. Apeirian detraxit pri eu. No solum accusam has. Ius ne harum mundi clita, eu pro tation audiam.
+Sed dicit necessitatibus in, id posse nominati eos. Ea vel dictas facilisi adipiscing, verear phaedrum sed ei, omnes oblique sanctus mea ex. Iudico pertinacia constituam cu eos. Te vel fugit libris, libris nemore no pri, graece oportere sea ea. Amet omnium epicuri cum te, sonet dignissim abhorreant pro ea, mei petentium constituam ad. Suscipit gloriatur necessitatibus has et.
+Eum at omnis omnesque percipitur, est ei melius scriptorem. At copiosae placerat vim, pro at sumo argumentum theophrastus, periculis theophrastus per et. No voluptatum contentiones eum, dolorem perfecto cu duo, duo eros nonumes ea. An has quaeque vivendum, iriure constituam ne nam. Electram maiestatis nec no.
+
+<meta content="Surfing with Jeff" name="title">
+<meta content="12/12/2016" name="date">
+<meta content="Brian Gonzalez" name="author_">
+        `,
+        filename: 'foo.post.md'
+      }
+    ]
+  })
+})
 module.exports = router
--- components/post/Post.vue
+++ components/post/Post.vue
@@ -1,9 +1,10 @@
 <template lang="html">
   <div>
-    <h1>
-      {{ post.meta.title }}
-    </h1>
-    <p><date>{{ post.meta.date | toIsoDate | moment("MMM D YYYY") }}</date></p>
+    <h1>{{ post.meta.title }}</h1>
+    <p>
+      <author v-if="post.meta.author">{{ post.meta.author }} / </author>
+      <date v-if="post.meta.date">{{ post.meta.date | toIsoDate | moment("MMM D YYYY") }}</date>
+      </p>
     <div v-html="post.content">
     </div>
   </div>
