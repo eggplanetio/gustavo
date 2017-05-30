5c995340c43a491a50e83f8dfd2e1e5b16e28927
Brian Gonzalez <me@briangonzalez.org>
2017-05-29 19:20:05 -0700

    fixed dockerfile

--- dummy/build-and-run.sh
+++ dummy/build-and-run.sh
@@ -5,6 +5,6 @@ cd ./dummy
 docker rmi -f gustavo-dummy
 docker rm -f gustavo-dummy
 docker build -t gustavo-dummy .
-docker run -d -p 4000:3000 --name gustavo-dummy gustavo-dummy
+docker run -p 4000:3000 --name gustavo-dummy gustavo-dummy
 
 # docker stop gustavo-dummy
