81ed6f276f121d85cabda8ccbbd0cd4306474788
Brian Gonzalez <me@briangonzalez.org>
2017-06-10 12:00:37 -0700

    remove apicache

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import express from 'express'
 import api from './api'
 import morgan from 'morgan'
 import config from '../nuxt.config.js'
-import apicache from 'apicache'
 
 config.dev = !(process.env.NODE_ENV === 'production')
 
@@ -11,18 +10,6 @@ const app = express()
 const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 3000
 
-const cache = apicache.middleware
-const cacheDuration = config.gustavo.cacheDuration || '30 minutes'
-apicache.options({
-  enabled: !config.dev
-})
-
-const shouldCache = req =>
-  !req.path.startsWith('/_nuxt') &&
-  !req.path.startsWith('/__webpack') &&
-  !req.path.startsWith('/api')
-
-app.use(cache(cacheDuration, shouldCache))
 app.use(morgan('tiny'))
 
 app.use('/favicon.ico', (req, res) => res.end())
